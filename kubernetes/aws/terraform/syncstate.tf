7968d340cac53bda239a62a21ce3bb1b4d5fee1e
billygk <gkrepper@gmail.com>
2022-04-13 09:21:47 +0200

    Update syncstate.tf

--- kubernetes/aws/terraform/syncstate.tf
+++ kubernetes/aws/terraform/syncstate.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt = false
-    bucket = "tehub-dev-terraform-state"
+    bucket = "dev-terraform-state"
     region = "us-east-1"
     key = "terraform.tfstate"
   }
